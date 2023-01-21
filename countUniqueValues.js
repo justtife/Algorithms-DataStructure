8db5999bafc04dbc4d75752467337265b14f823f
Justtife <farinubolu@gmail.com>
2023-01-21 12:49:18 +0100

    Updated count unique Values

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,6 +1,7 @@
 function countUniqueValues(arr) {
+  //Space Complexity - o(1)
   if (arr.length === 0) return 0;
-  let n = 0;
+  let n = 1;
   let num = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] !== num) {
@@ -8,7 +9,13 @@ function countUniqueValues(arr) {
       n++;
     }
   }
-  return n + 1;
+  return n;
+  // Space Complexity - o(n)
+  // let numOBJ = {};
+  // for (let i of arr) {
+  //   numOBJ[i] = (numOBJ[i] || 0) + 1;
+  // }
+  // return Object.keys(numOBJ).length;
 }
 console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // 2
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
